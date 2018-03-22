ed507015f74c35a49081e2e6b528a4fb025430d2
Victor Sanchez Gassull <victor.sanchez.gassull@a5s113pc32.fib.upc.es>
2018-03-22 10:13:11 +0100

    modular exp

--- Divide_and_Conquer/P29212/P29212.cc
+++ Divide_and_Conquer/P29212/P29212.cc
@@ -1,23 +1,31 @@
 #include <iostream>
-
 using namespace std;
 
-int modular_pow(int base, int exponent, int modulus)
-{
-	int result = 1;
-	while (exponent > 0)
-	{
-		if (exponent % 2 == 1)
-			result = (result * base) % modulus;
+//k parell: n^k = (n^(k/2)))²
+// k imparell: n^k = (n^(k/2))²*n
+
+int i_pow(int base, int exponent, int modul) {
+	/*int result = 1;
+	while (exponent > 0) {
+		if (exponent % 2 == 1) 
+			result = (result * base) % modul;
 		exponent = exponent >> 1;
-		base = (base * base) % modulus;
+		base = (base * base) % modul;
 	}
+	return result;*/
+	
+	if (exponent == 0)
+		return 1;
+	int result = i_pow(base,exponent/2,modul);
+	result = (result * result) % modul;
+	if (exponent % 2)
+		result = (result * base) % modul;
 	return result;
 }
 
 int main() {
 	int n, k, m;
 	while (cin >> n >> k >> m) {
-		cout << modular_pow(n, k, m) << endl;
+		cout << i_pow(n, k, m) << endl;
 	}
 }
--- Divide_and_Conquer/P74219/P74219.cc
+++ Divide_and_Conquer/P74219/P74219.cc
@@ -2,7 +2,21 @@
 #include <vector>
 using namespace std;
 
+bool read(mat,size) {
+	for (int i = 0; i < size; ++i)
+		for (int j = 0; j < size; ++j)
+			cin >> mat[i][j];
+	return true;
+}
 
 int main() {
+	
+	vector <vector <int> > mat(vector <int> (2));
+	int size = 2;
+	while (read(mat,2)) {
+		int n,m; cin >> n >> m;
+		
+	}
+	
 	
 }
