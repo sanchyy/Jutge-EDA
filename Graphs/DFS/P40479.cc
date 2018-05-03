4e922b01f6b17c2fddd9cad0d300bfe2f8018cbb
Sanchy <vsanchez.gassull@gmail.com>
2018-05-03 12:02:23 +0200

    painting board

--- Graphs/DFS/P40479.cc
+++ Graphs/DFS/P40479.cc
@@ -4,37 +4,57 @@ using namespace std;
 
 using VB = vector <bool>;
 using VC = vector <char>;
+using VVB = vector <VB>;
 using VVC = vector <VC>;
 
 int n,m;
-VB visit;
+VVB visit;
 VVC G;
 
+
+void write() {
+    for (int i = 0; i < n; ++i){
+        for (int j = 0; j < m; ++j)
+            cout << G[i][j];
+        cout << endl;
+    }
+}
+
 void DFS(int x, int y, char c) {
+
     if (x >= 0 and x < n and y >= 0 and y < m) {
         if (G[x][y] == '#')
             return;
-        G[x][y] = c;
-        DFS(x+1,y,c);
-        DFS(x-1,y,c);
-        DFS(x,y+1,c);
-        DFS(x,y-1,c);
+        if (not visit[x][y]) {
+            visit[x][y] = true;
+            G[x][y] = c;
+            DFS(x+1,y,c);
+            DFS(x-1,y,c);
+            DFS(x,y+1,c);
+            DFS(x,y-1,c);
+        }
+
     }
 }
 
 void solu() {
     for (int i = 0; i < n; ++i){
         for (int j = 0; j < m; ++j) {
-            if ((G[i][j] != '#' and G[i][j] != '.'))
+            if (G[i][j] == '#')
+                visit[i][j] = true;
+            else if ((G[i][j] != '.') and not visit[i][j]){
                 DFS(i,j,G[i][j]);
+            }
+
         }
     }
 }
 
+
 int main() {
     while (cin >> n >> m) {
         G = VVC(n,VC(m));
-        visit = VB(n,false);
+        visit = VVB(n,VB(m,false));
         for (int i = 0; i < n; ++i)
             for (int j = 0; j < m; ++j)
                 cin >> G[i][j];
@@ -46,5 +66,6 @@ int main() {
                 cout << G[i][j];
             cout << endl;
         }
+        cout << endl;
     }
 }
